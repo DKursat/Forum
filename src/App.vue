d94b893477522cdb38568510918beaa67918ad32
Kursat <75574588+DKursat@users.noreply.github.com>
2022-03-12 19:31:02 +0100

    Summary

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,7 @@
   <Header/>
   
   
-
-  <div class="headline"><h1>FORUM für alles</h1></div>
-'<router-link to="/sidebar">Sidebar</router-link>
- 
-  <router-view/>
-
-
+  
 </template>
 
 
@@ -31,39 +25,5 @@ export default {
 
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 2px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-
-
-#headline{
-  background-color: #2c3e50;
-  color: black;
-  
-  margin-bottom: 0%;
-  
-}
-#router-link{
-  margin-left: 20%;
-  margin-right: 20%;
-}
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,6 @@
 <template>
 
-  <div id="nav">
-  | <router-link to="/">News</router-link> |
-    <router-link to="/">Games</router-link> |
-    <router-link to="/">Movies</router-link> |
-    <router-link to="/">Communtiy</router-link> |
-  </div>
+  
 </template>
 
 <script>

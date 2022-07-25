ccf20b2c0d465087f5f987f441ec40c35a42fbe0
Kursat <75574588+DKursat@users.noreply.github.com>
2022-07-25 22:55:26 +0200

    Summary

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import Oruga from '@oruga-ui/oruga-next'
-import '@oruga-ui/oruga-next/dist/oruga.css'
-createApp(App).use(store).use(router).use(Oruga).mount('#app')
-

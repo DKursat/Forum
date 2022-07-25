2548c441c721f172d83f6bfba6f7dd271412fe96
Kursat <75574588+DKursat@users.noreply.github.com>
2022-07-25 23:37:32 +0200

    Summary

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+<script>
+import homepage from "../components/Home.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <homepage />
   </main>
 </template>
